3c3783eb8e2559679b09c116ecd820bad403b099
Sébastien Brochet <sebastien.brochet@cern.ch>
2015-08-17 14:26:15 +0200

    Update SSH public keys

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'blinkseb@madalynn.eu'
 license          'All rights reserved'
 description      'Installs/Configures madalynn'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 depends 'user'
 depends 'sudo'
