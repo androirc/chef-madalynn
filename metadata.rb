3ce130c2bc0c05462f041efec8ab01504d34146b
Sébastien Brochet <sebastien.brochet@cern.ch>
2016-07-23 17:44:52 +0200

    Move to Berkshelf

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,4 @@ maintainer_email 'blinkseb@madalynn.eu'
 license          'All rights reserved'
 description      'Installs/Configures madalynn'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.5'
-
-depends 'user'
-depends 'sudo'
-depends 'chef-vault'
-depends 'unattended-upgrades'
-depends 'hostsfile'
+version          '0.1.6'
