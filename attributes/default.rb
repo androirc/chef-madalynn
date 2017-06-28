0a2c6e75bdf763a915e1503dfc5de3f700154a73
Sébastien Brochet <sebastien.brochet@cern.ch>
2017-06-28 19:13:34 +0200

    Fix unattended-upgrades configuration

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default['madalynn']['bouncer_ip'] = '10.10.10.254'
 
-override['apt']['unattended-upgrades']['enable'] = true
-override['apt']['unattended-upgrades']['remove_unused_dependencies'] = true
+override['apt']['unattended_upgrades']['enable'] = true
+override['apt']['unattended_upgrades']['remove_unused_dependencies'] = true
 
 override['sshd']['sshd_config']['ListenAddress'] = '0.0.0.0'
 override['sshd']['sshd_config']['PermitRootLogin'] = 'without-password'
