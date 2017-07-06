45f2ccca000d9894d2fd532b568f3c5ff24b68c2
Sébastien Brochet <sebastien.brochet@cern.ch>
2017-07-06 18:06:17 +0200

    More allowed origins for unattended-upgrades

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,6 +7,9 @@ override['apt']['unattended_upgrades']['allowed_origins'] = [
   '${distro_id}:${distro_codename}',
   '${distro_id}:${distro_codename}-security',
   '${distro_id}:${distro_codename}-updates',
+  'MariaDB:',
+  'LP-PPA-formorer-icinga:${distro_codename}',
+  'Node Source:'
 ]
 
 override['sshd']['sshd_config']['ListenAddress'] = '0.0.0.0'
